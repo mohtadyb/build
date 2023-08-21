f6a69699cecb833d44e3449a0ce86a491067d837
spc.p.p <mohtadyb@gmail.com>
2023-08-21 09:00:12 +0200

    first commit

---
+++ precache-manifest.d0ccd7b7d87d01bb7ecceda3771db645.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86474adb44cb6111e6320d0a9e185f09",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "cbcae8696fc0b9cbc78c",
+    "url": "/react-invoice-generator/static/css/2.903596ff.chunk.css"
+  },
+  {
+    "revision": "c223d192ce363f104ec5",
+    "url": "/react-invoice-generator/static/css/main.f5b90593.chunk.css"
+  },
+  {
+    "revision": "cbcae8696fc0b9cbc78c",
+    "url": "/react-invoice-generator/static/js/2.d8b0e08e.chunk.js"
+  },
+  {
+    "revision": "0a3e9ef2294d8e25a0972ff1e7ceed30",
+    "url": "/react-invoice-generator/static/js/2.d8b0e08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c223d192ce363f104ec5",
+    "url": "/react-invoice-generator/static/js/main.2fef17a7.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "c398871b85c6bcecbee391b4b247d38f",
+    "url": "/react-invoice-generator/static/media/Shamel-book.c398871b.ttf"
+  },
+  {
+    "revision": "a610f9cd73588e17ebab5417216dbf86",
+    "url": "/react-invoice-generator/static/media/footer.a610f9cd.jpeg"
+  },
+  {
+    "revision": "a4f41eff32f6751baca4ecfb3560e8f8",
+    "url": "/react-invoice-generator/static/media/header.a4f41eff.jpeg"
+  }
+]);
